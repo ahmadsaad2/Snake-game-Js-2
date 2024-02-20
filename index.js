b19bd48f4d230e5662bc4482741e7d937d406e29
ahmadsaad2 <137313539+ahmadsaad2@users.noreply.github.com>
2024-02-20 18:13:41 +0200

    js #4

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ const controls=document.querySelector(".control");
 let gameOver=false;
 let foodx,foody;
 let snakex=5,snakey=5;
-let velocityX=0,velocityY=0;
+let velocityX=0;
+let velocityY=0;
 let snakeBody=[];
 let setIntervalid;
 let score=0;
@@ -26,3 +27,21 @@ const handelGame =() =>{
     alert("Game over press ok to replay");
     location.reload();
 }
+
+const changeDirection =e =>{
+    if(e.key === "ArrowUp" && velocityY !=1 ){
+        velocityX=0;
+        velocityY=-1;
+    }
+    else if(e.key === "Arrowdown" && velocityY !=-1){
+        velocityX=0;
+        velocityY=1;
+    }else if(e.key === "ArrowLeft" && velocityY !=-1){
+        velocityX=-1;
+        velocityY=0;
+    }else if(e.key === "Arrowright" && velocityY !=-1){
+        velocityX=1;
+        velocityY=0;
+    }
+}
+controls.forEach(button => button.addEventListener("click",()=>changeDirection({key:button.dataset.key})));
